b43662dc33a77068cd6be1a2fefadae474edcf16
0x3C-0x3E <rbng.0094@gmail.com>
2025-03-31 17:49:59 +0200

    fixed everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys, heapq
 
-DEBUG = True
+
 
 
 class TreeNode:
@@ -33,7 +33,8 @@ class TreeNode:
 			return self.char
 
 
-ROOT_NODE = TreeNode(0)
+DEBUG = True
+PEARL_COUNT = 3
 
 def print_debug(message):
 	if not DEBUG:
@@ -72,7 +73,23 @@ def sort_dict_by_value(dictonary, should_reverse = True):
 	return dictonary
 
 def find_n_smallest(input_list, n):
-    return heapq.nsmallest(n, input_list, key=lambda node: node.node_cost)
+	return heapq.nsmallest(n, input_list, key=lambda node: node.node_cost)
+
+def find_element_path(lst, target, path=None):
+    if path is None:
+        path = []
+
+    for index, item in enumerate(lst):
+        new_path = path + [index]
+
+        if item == target:
+            return new_path
+        elif isinstance(item, list):
+            result = find_element_path(item, target, new_path)
+            if result:
+                return result
+
+    return None
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
@@ -82,6 +99,10 @@ if __name__ == "__main__":
 	lines = get_file_contents(sys.argv[1])
 
 	sorted_dict = sort_dict_by_value(get_char_probability_dict(lines[2]))
+
+	PEARL_COUNT = int(lines[0])
+
+	print_debug(f"Pearl Count: {PEARL_COUNT}")
 
 	print_debug(f"sorted dict {sorted_dict}")
 
@@ -95,7 +116,7 @@ if __name__ == "__main__":
 	while top_most_nodes > 1:
 		print_debug("New optimisation phase")
 
-		smallest_cost_nodes = find_n_smallest(tree_nodes, 2)
+		smallest_cost_nodes = find_n_smallest(tree_nodes, PEARL_COUNT)
 
 		parent_node_cost = 0
 		
@@ -116,10 +137,9 @@ if __name__ == "__main__":
 		print_debug(f"Newly generated parent_node: ['{parent_node.char}'], [{parent_node.node_cost}], {parent_node.get_char()}")
 
 		#TODO CHANGE THAT
-		top_most_nodes = top_most_nodes - 2 + 1
+		top_most_nodes = top_most_nodes - PEARL_COUNT + 1
 
 		if top_most_nodes == 1:
-			ROOT_NODE = parent_node
 			print_debug("reached root node")
 			break
 
@@ -129,3 +149,26 @@ if __name__ == "__main__":
 		#debug statement to check if they werent garbage collected
 		for child_node in parent_node.child_nodes:
 			print_debug(f"child node of new parent_node: ['{child_node.char}'], [{child_node.node_cost}]")
+
+	translation_dict = {}
+
+	for character in sorted_dict.keys():
+		nested_list = parent_node.get_char()
+		path = find_element_path(nested_list, character)
+		print(f"[RESULT] Character: {character} => {path}")
+		translation_dict[character] = path
+
+	for key, value in translation_dict.items():
+		print_debug(f"Translation Dict: {key} : {value}")
+
+	message = ''
+	message_len = 0
+	for character in lines[2]:
+		string = ''
+		for byte in translation_dict[character]:
+			string += str(byte)
+		message += string
+		message_len += len(translation_dict[character])
+
+	print(f"\n[FINAL] Message => {message}, len => {message_len}")
+
